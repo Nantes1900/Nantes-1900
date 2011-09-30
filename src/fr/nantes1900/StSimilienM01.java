953813b706b3f04fa431eda5bd2219f2bb4acaf0
Daniel Lefevre <dan221@msn.com>
2011-09-30 17:31:11 +0200

    Test fonctionnement.

--- src/fr/nantes1900/StSimilienM01.java
+++ src/fr/nantes1900/StSimilienM01.java
@@ -5,24 +5,25 @@ import fr.nantes1900.models.extended.Town;
 /**
  * Implements a main class. Here it is the treatment of the first part of St
  * Similien mock-up.
- * 
  * @author Daniel Lefevre
  */
-public final class StSimilienM01 {
+public final class StSimilienM01
+{
 
     /**
      * Private constructor.
      */
-    private StSimilienM01() {
+    private StSimilienM01()
+    {
     }
 
     /**
      * Main function.
-     * 
      * @param args
      *            arguments (the program does not take them into account)
      */
-    public static void main(final String[] args) {
+    public static void main(final String[] args)
+    {
 
         // Create new town
         final Town town = new Town();
