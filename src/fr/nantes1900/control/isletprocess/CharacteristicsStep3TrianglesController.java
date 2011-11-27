49fd998c2575fa31f488ec0245d7fce1b7253d5b
Camille Bouquet <bouquetcamille@hotmail.fr>
2011-11-27 15:35:03 +0100

    Puts launch action in each characteristics controller which knows exactly which action to perform

--- src/fr/nantes1900/control/isletprocess/CharacteristicsStep3TrianglesController.java
+++ src/fr/nantes1900/control/isletprocess/CharacteristicsStep3TrianglesController.java
@@ -12,15 +12,15 @@ import javax.swing.JOptionPane;
 import fr.nantes1900.constants.ActionTypes;
 import fr.nantes1900.constants.Characteristics;
 import fr.nantes1900.models.basis.Triangle;
+import fr.nantes1900.models.islets.buildings.exceptions.InvalidCaseException;
 import fr.nantes1900.view.isletprocess.CharacteristicsStep2View;
 import fr.nantes1900.view.isletprocess.CharacteristicsStep3TrianglesView;
 
 /**
  * Characteristics panel for the second step of process of an islet. User can
- * select one or more triangles and modifies the type they belong to : building
- * or ground.
- * @author Camille
- * @author Luc
+ * select one or more triangles and delete them.
+ * @author Camille Bouquet
+ * @author Luc Jallerat
  */
 public class CharacteristicsStep3TrianglesController extends
         CharacteristicsController
@@ -48,37 +48,14 @@ public class CharacteristicsStep3TrianglesController extends
                 if (((CharacteristicsStep3TrianglesView) cView)
                         .isDeleteSelected())
                 {
-                    CharacteristicsStep3TrianglesController.this.parentController
-                    .launchAction(3, ActionTypes.REMOVE,
-                            Characteristics.SELECTION_TYPE_TRIANGLE);
-                } else
-                {
-                    String typeChosen = ((CharacteristicsStep3TrianglesView) cView)
-                            .getElementSelected();
-                    
-                    int actionType = -1;
-                    switch (typeChosen)
+                    try
                     {
-                        case Characteristics.TYPE_NOISE:
-                            actionType = ActionTypes.TURN_TO_NOISE;
-                            break;
-                            
-                        case Characteristics.TYPE_BUILDING:
-                            actionType = ActionTypes.TURN_TO_BUILDING;
-                            break;
-                    }
-                    
-                    if (actionType != -1)
-                    {
-                        CharacteristicsStep3TrianglesController.this.parentController
-                        .launchAction(3, actionType,
-                                Characteristics.SELECTION_TYPE_TRIANGLE);
-                    } else
+                        CharacteristicsStep3TrianglesController.this.parentController.getBiController().action3(trianglesList, ActionTypes.REMOVE);
+                    } catch (InvalidCaseException e)
                     {
                         JOptionPane.showMessageDialog(cView,
-                                "Le type choisi est incorrrect",
-                                "Validation impossible",
-                                JOptionPane.ERROR_MESSAGE);
+                                "L'action choisie est incorrecte.",
+                                "Validation impossible", JOptionPane.ERROR_MESSAGE);
                     }
                 }
             }
--- src/fr/nantes1900/control/isletprocess/CharacteristicsStep5Controller.java
+++ src/fr/nantes1900/control/isletprocess/CharacteristicsStep5Controller.java
@@ -7,9 +7,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 import fr.nantes1900.constants.ActionTypes;
 import fr.nantes1900.constants.Characteristics;
 import fr.nantes1900.models.extended.Surface;
+import fr.nantes1900.models.islets.buildings.exceptions.InvalidCaseException;
+import fr.nantes1900.models.islets.buildings.exceptions.UnCompletedParametersException;
 import fr.nantes1900.view.isletprocess.CharacteristicsStep2View;
 import fr.nantes1900.view.isletprocess.CharacteristicsStep5View;
 
@@ -42,12 +46,40 @@ public class CharacteristicsStep5Controller extends CharacteristicsController
                               
                 if (((CharacteristicsStep5View) cView).isMergeSelected())
                 {
-                    CharacteristicsStep5Controller.this.parentController.launchAction(5, ActionTypes.MERGE, Characteristics.SELECTION_TYPE_ELEMENT);
+                    try
+                    {
+                        CharacteristicsStep5Controller.this.parentController.getBiController().action5(surfacesList, ActionTypes.MERGE);
+                    } catch (InvalidCaseException e1)
+                    {
+                        JOptionPane.showMessageDialog(cView,
+                                "Le type choisi est incorrrect",
+                                "Validation impossible", JOptionPane.ERROR_MESSAGE);
+                    } catch (UnCompletedParametersException e2)
+                    {
+                        // TODO : remove when implemented
+                        JOptionPane.showMessageDialog(cView,
+                                "Non implémenté encore",
+                                "Validation impossible", JOptionPane.ERROR_MESSAGE);
+                    }
                 } 
 
                 if (((CharacteristicsStep5View) cView).isRemoveSelected())
                 {
-                    CharacteristicsStep5Controller.this.parentController.launchAction(5, ActionTypes.TURN_TO_NOISE, Characteristics.SELECTION_TYPE_ELEMENT);
+                    try
+                    {
+                        CharacteristicsStep5Controller.this.parentController.getBiController().action5(surfacesList, ActionTypes.TURN_TO_NOISE);
+                    } catch (InvalidCaseException e1)
+                    {
+                        JOptionPane.showMessageDialog(cView,
+                                "Le type choisi est incorrrect",
+                                "Validation impossible", JOptionPane.ERROR_MESSAGE);
+                    } catch (UnCompletedParametersException e2)
+                    {
+                        // TODO : remove when implemented
+                        JOptionPane.showMessageDialog(cView,
+                                "Non implémenté encore",
+                                "Validation impossible", JOptionPane.ERROR_MESSAGE);
+                    }
                 } 
             }
         });
