404b98643173341a3597850b4f8ba5b47aa53ae6
Daniel Lefevre <daniel.lefevre@eleves.ec-nantes.fr>
2011-12-01 21:33:13 +0100

    Style corrections.

--- src/fr/nantes1900/view/display3d/PolygonView.java
+++ src/fr/nantes1900/view/display3d/PolygonView.java
@@ -13,154 +13,164 @@ import javax.vecmath.Vector3f;
  * display the surface simplified as a polygon.
  * @author Nicolas Bouillon Siju Wu
  */
-public class PolygonView extends TriangleFanArray {
+public class PolygonView extends TriangleFanArray
+{
+    /**
+     * The polygon of the thing displayed.
+     */
+    private Polygon         polygon;
+    /**
+     * Indicate if the polygon is selected. TODO
+     */
+    private boolean         selected;
+    /**
+     * The center of the polygon.
+     */
+    private Point           centroid;
 
-	/**
-	 * The polygon of the thing displayed.
-	 */
-	private Polygon polygon;
-	/**
-	 * Indicate if the polygon is selected.
-	 * TODO
-	 */
-	private boolean selected;
-	/**
-	 * The center of the polygon.
-	 */
-	private Point centroid;
-	
-	public static final int POLYGON_FACES_COUNT=2;
+    /**
+     * FIXME : problem : the number of faces is different for each polygons.
+     */
+    public static final int POLYGON_FACES_COUNT = 2;
 
-	/**
-	 * Constructor of the class PolygonView.
-	 * @param poly
-	 *                The polygon of the thing displayed.
-	 */
-	public PolygonView(final Polygon poly) {
-		super(poly.getPointList().size() * POLYGON_FACES_COUNT, GeometryArray.COORDINATES
-				| GeometryArray.COLOR_3 | GeometryArray.NORMALS
-				| GeometryArray.TEXTURE_COORDINATE_2, new int[] { poly
-				.getPointList().size() * POLYGON_FACES_COUNT });
+    /**
+     * Constructor of the class PolygonView.
+     * @param poly
+     *            The polygon of the thing displayed.
+     */
+    public PolygonView(final Polygon poly)
+    {
+        // FIXME : instead of POLYOGN_FACES_COUNT, put poly.getNumEdges() or I
+        // don't know why to count the number of faces.
+        super(poly.getPointList().size() * POLYGON_FACES_COUNT,
+                GeometryArray.COORDINATES | GeometryArray.COLOR_3
+                        | GeometryArray.NORMALS
+                        | GeometryArray.TEXTURE_COORDINATE_2, new int[] {poly
+                        .getPointList().size() * POLYGON_FACES_COUNT});
 
-		this.polygon = poly;
-		this.centroid = new Point(0, 0, 0);
-		for (Point point : poly.getPointList()) {
-			this.centroid.setX(this.centroid.getX() + point.getX());
-			this.centroid.setY(this.centroid.getY() + point.getY());
-			this.centroid.setZ(this.centroid.getZ() + point.getZ());
-		}
+        this.polygon = poly;
+        this.centroid = new Point(0, 0, 0);
+        for (Point point : poly.getPointList())
+        {
+            this.centroid.setX(this.centroid.getX() + point.getX());
+            this.centroid.setY(this.centroid.getY() + point.getY());
+            this.centroid.setZ(this.centroid.getZ() + point.getZ());
+        }
 
-		this.centroid.setX(this.centroid.getX() / (poly.getPointList().size()));
-		this.centroid.setY(this.centroid.getY() / (poly.getPointList().size()));
-		this.centroid.setZ(this.centroid.getZ() / (poly.getPointList().size()));
+        this.centroid.setX(this.centroid.getX() / (poly.getPointList().size()));
+        this.centroid.setY(this.centroid.getY() / (poly.getPointList().size()));
+        this.centroid.setZ(this.centroid.getZ() / (poly.getPointList().size()));
 
-		//TODO
-		this.selected = false;
+        // TODO
+        this.selected = false;
 
-		Point3d[] vertex = new Point3d[poly.getPointList().size() * POLYGON_FACES_COUNT];
-		for (int i = 0; i < poly.getPointList().size(); i++) {
-			vertex[i] = new Point3d(poly.getPointList().get(i).getX(), poly
-					.getPointList().get(i).getY(), poly.getPointList().get(i)
-					.getZ());
-			vertex[poly.getPointList().size() * POLYGON_FACES_COUNT - 1 - i] = new Point3d(poly
-					.getPointList().get(i).getX(), poly.getPointList().get(i)
-					.getY(), poly.getPointList().get(i).getZ());
-		}
-		this.setCoordinates(0, vertex);
+        Point3d[] vertex = new Point3d[poly.getPointList().size()
+                * POLYGON_FACES_COUNT];
+        for (int i = 0; i < poly.getPointList().size(); i++)
+        {
+            vertex[i] = new Point3d(poly.getPointList().get(i).getX(), poly
+                    .getPointList().get(i).getY(), poly.getPointList().get(i)
+                    .getZ());
+            vertex[poly.getPointList().size() * POLYGON_FACES_COUNT - 1 - i] = new Point3d(
+                    poly.getPointList().get(i).getX(), poly.getPointList()
+                            .get(i).getY(), poly.getPointList().get(i).getZ());
+        }
+        this.setCoordinates(0, vertex);
 
-		Vector3f[] normal = new Vector3f[poly.getPointList().size() * POLYGON_FACES_COUNT];
-		for (int i = 0; i < poly.getPointList().size(); i++) {
-			normal[i] = convertNormal(this.polygon);
-			normal[poly.getPointList().size() * POLYGON_FACES_COUNT - 1 - i] = reverseConvertNormal(this.polygon);
-		}
+        Vector3f[] normal = new Vector3f[poly.getPointList().size()
+                * POLYGON_FACES_COUNT];
+        for (int i = 0; i < poly.getPointList().size(); i++)
+        {
+            normal[i] = convertNormal(this.polygon);
+            normal[poly.getPointList().size() * POLYGON_FACES_COUNT - 1 - i] = reverseConvertNormal(this.polygon);
+        }
 
-		this.setNormals(0, normal);
-	}
+        this.setNormals(0, normal);
+    }
 
-	/**
-	 * Get the polygon displayed.
-	 * @return polygon
-	 *             the polygon displayed.
-	 */
-	public Polygon getPolygon() {
-		return this.polygon;
-	}
+    /**
+     * Get the polygon displayed.
+     * @return polygon the polygon displayed.
+     */
+    public final Polygon getPolygon()
+    {
+        return this.polygon;
+    }
 
-	/**
-	 * Set the polygon to be displayed.
-	 * @param polygonIn
-	 *             the polygon to set.
-	 */
-	public void setPolyline(Polygon polygonIn) {
-		this.polygon = polygonIn;
-	}
+    /**
+     * Set the polygon to be displayed.
+     * @param polygonIn
+     *            the polygon to set.
+     */
+    public final void setPolygon(final Polygon polygonIn)
+    {
+        this.polygon = polygonIn;
+    }
 
-	/**
-	 * To know if the polygon is selected.
-	 * @return selected
-	 *             the condition of selection.
-	 */
-	public boolean isSelected() {
-		return this.selected;
-	}
+    /**
+     * To know if the polygon is selected.
+     * @return selected the condition of selection.
+     */
+    public final boolean isSelected()
+    {
+        return this.selected;
+    }
 
-	/**
-	 * Set the condition of seleciton of this polygon.
-	 * @param selectedIn 
-	 *             the condition of selection to set.
-	 */
-	public void setSelected(boolean selectedIn) {
-		this.selected = selectedIn;
-	}
+    /**
+     * Select the polygon.
+     */
+    public final void select()
+    {
+        this.selected = true;
 
-	/**
-	 *  Select the polygon.
-	 */
-	public void select() {
-		this.selected = true;
+    }
 
-	}
+    /**
+     * Unselect the polygon.
+     */
+    public final void unselect()
+    {
+        this.selected = false;
 
-	/**
-	 *  Unselect the polygon.
-	 */
-	public void unselect() {
-		this.selected = false;
+    }
 
-	}
+    /**
+     * Converts the normal of the polygon (Vector3d) in a Vector3f.
+     * @param polygon
+     *            the polygon to get the normal from.
+     * @return the normal as a Vector3d
+     */
+    public static Vector3f convertNormal(final Polygon polygon)
+    {
+        Vector3f normalFloat = new Vector3f((float) polygon.getNormal().getX(),
+                (float) polygon.getNormal().getY(), (float) polygon.getNormal()
+                        .getZ());
+        return normalFloat;
+    }
 
-	/**
-	 * Converts the normal of the polygon (Vector3d) in a Vector3f.
-	 * @param polygon 
-	 *           the polygon to get the normal from.
-	 * @return the normal as a Vector3d
-	 */
-	public static Vector3f convertNormal(Polygon polygon) {
-		Vector3f nromalFloat = new Vector3f((float) polygon.getNormal().getX(),
-				(float) polygon.getNormal().getY(), (float) polygon.getNormal()
-						.getZ());
-		return nromalFloat;
-	}
+    /**
+     * Converts the reverse of the normal of the polygon (Vector3d) in a
+     * Vector3f.
+     * @param polygon
+     *            the polygon to get the normal from.
+     * @return the reverse of the normal as a Vector3d
+     */
+    public static Vector3f reverseConvertNormal(final Polygon polygon)
+    {
+        Vector3f normalFloat = new Vector3f(
+                -(float) polygon.getNormal().getX(), -(float) polygon
+                        .getNormal().getY(), -(float) polygon.getNormal()
+                        .getZ());
+        return normalFloat;
+    }
 
-	/**
-	 * Converts the reverse of the normal of the polygon (Vector3d) in a Vector3f.
-	 * @param polygon
-	 *           the polygon to get the normal from.
-	 * @return the reverse of the normal as a Vector3d
-	 */
-	public static Vector3f reverseConvertNormal(Polygon polygon) {
-		Vector3f nromalFloat = new Vector3f(
-				-(float) polygon.getNormal().getX(), -(float) polygon
-						.getNormal().getY(), -(float) polygon.getNormal()
-						.getZ());
-		return nromalFloat;
-	}
-
-	/**
-	 * @return centroid The center of the polygon.
-	 */
-	public Point getCentroid() {
-		return this.centroid;
-	}
+    /**
+     * TODO.
+     * @return centroid The center of the polygon.
+     */
+    public final Point getCentroid()
+    {
+        return this.centroid;
+    }
 
 }
--- src/fr/nantes1900/view/display3d/SurfaceView.java
+++ src/fr/nantes1900/view/display3d/SurfaceView.java
@@ -22,156 +22,168 @@ import fr.nantes1900.models.extended.Surface;
  * U3DController tells if the meshView or the polygonView is to be displayed. It
  * is possible for a surfaceView instance to have its meshView polygonView set
  * to null (for example in the first steps, the polygonView is null)
- * 
  * @author Siju Wu, Nicolas Bouillon
  */
-public class SurfaceView extends Shape3D {
+public class SurfaceView extends Shape3D
+{
+    /**
+     * The material of a surface selected.
+     */
+    public static final Material MATERIAL_SELECTED   = new Material(
+                                                             new Color3f(0.2f,
+                                                                     0f, 0f),
+                                                             new Color3f(0.0f,
+                                                                     0.0f, 0.0f),
+                                                             new Color3f(
+                                                                     Color.blue),
+                                                             new Color3f(
+                                                                     Color.blue),
+                                                             64);
 
-	/**
-	 * The material of a surface selected.
-	 */
-	public static final Material MATERIAL_SELECTED = new Material(new Color3f(
-			0.2f, 0f, 0f), new Color3f(0.0f, 0.0f, 0.0f), new Color3f(
-			Color.blue), new Color3f(Color.blue), 64);
+    /**
+     * The material of a surface non-selected.
+     */
+    public static final Material MATERIAL_UNSELECTED = new Material(
+                                                             new Color3f(0.2f,
+                                                                     0f, 0f),
+                                                             new Color3f(0.0f,
+                                                                     0.0f, 0.0f),
+                                                             new Color3f(0.7f,
+                                                                     0f, 0f),
+                                                             new Color3f(0.7f,
+                                                                     0f, 0f),
+                                                             64);
 
-	/**
-	 * The material of a surface non-selected.
-	 */
-	public static final Material MATERIAL_UNSELECTED = new Material(
-			new Color3f(0.2f, 0f, 0f), new Color3f(0.0f, 0.0f, 0.0f),
-			new Color3f(0.7f, 0f, 0f), new Color3f(0.7f, 0f, 0f), 64);
+    /**
+     * The surface linked to this view.
+     */
+    private Surface              surface;
+    /**
+     * The mesh to be displayed.
+     */
+    private MeshView             meshView;
+    /**
+     * The polygon to be displayed.
+     */
+    private PolygonView          polygonView;
+    /**
+     * The appearance of the surface.
+     */
+    private Appearance           appearance          = new Appearance();
 
-	/**
-	 * The surface linked to this view.
-	 */
-	private Surface surface;
-	/**
-	 * The mesh to be displayed.
-	 */
-	private MeshView meshView;
-	/**
-	 * The polygon to be displayed.
-	 */
-	private PolygonView polygonView;
-	/**
-	 * The appearance of the surface.
-	 */
-	private Appearance appearance = new Appearance();
+    /**
+     * Constructor of the surfaceView.
+     * @param surfaceIn
+     *            the surface to build from
+     */
+    public SurfaceView(final Surface surfaceIn)
+    {
+        super();
 
-	/**
-	 * Constructor of the surfaceView.
-	 * 
-	 * @param surfaceIn
-	 *            the surface to build from
-	 */
-	public SurfaceView(final Surface surfaceIn) {
-		super();
+        this.surface = surfaceIn;
 
-		this.surface = surfaceIn;
+        this.setCapability(Shape3D.ALLOW_APPEARANCE_READ);
+        this.setCapability(Shape3D.ALLOW_APPEARANCE_WRITE);
 
-		this.setCapability(Shape3D.ALLOW_APPEARANCE_READ);
-		this.setCapability(Shape3D.ALLOW_APPEARANCE_WRITE);
+        this.appearance.setCapability(Appearance.ALLOW_MATERIAL_READ);
+        this.appearance.setCapability(Appearance.ALLOW_MATERIAL_WRITE);
 
-		this.appearance.setCapability(Appearance.ALLOW_MATERIAL_READ);
-		this.appearance.setCapability(Appearance.ALLOW_MATERIAL_WRITE);
-		
-		//Read the texture.
-		TextureLoader loader = new TextureLoader("texture.jpg", null);
+        // Read the texture.
+        TextureLoader loader = new TextureLoader("texture.jpg", null);
         ImageComponent2D image = loader.getImage();
         Texture2D texture = new Texture2D(Texture.BASE_LEVEL, Texture.RGB,
                 image.getWidth(), image.getHeight());
         texture.setImage(0, image);
-        
-        //Set the texture.
+
+        // Set the texture.
         this.appearance.setTexture(texture);
-		
-		TextureAttributes texAtt = new TextureAttributes();
-		texAtt.setTextureMode(TextureAttributes.MODULATE);
-		this.appearance.setTextureAttributes(texAtt);
 
-		// Ignores vertex colors to allow diffuse colors.
-		RenderingAttributes rendering = new RenderingAttributes();
-		rendering.setIgnoreVertexColors(true);
-		this.appearance.setRenderingAttributes(rendering);
+        TextureAttributes texAtt = new TextureAttributes();
+        texAtt.setTextureMode(TextureAttributes.MODULATE);
+        this.appearance.setTextureAttributes(texAtt);
 
-		this.setMaterial(MATERIAL_UNSELECTED);
-		this.setAppearance(this.appearance);	}
+        // Ignores vertex colors to allow diffuse colors.
+        RenderingAttributes rendering = new RenderingAttributes();
+        rendering.setIgnoreVertexColors(true);
+        this.appearance.setRenderingAttributes(rendering);
 
-	/**
-	 * Gets the mesh of triangle in the surfaceView.
-	 * 
-	 * @return this.meshView
-	 */
+        this.setMaterial(MATERIAL_UNSELECTED);
+        this.setAppearance(this.appearance);
+    }
 
-	public final MeshView getMeshView() {
-		return this.meshView;
-	}
+    /**
+     * Gets the mesh of triangle in the surfaceView.
+     * @return this.meshView
+     */
 
-	/**
-	 * Gets the polygon in the surfaceView.
-	 * 
-	 * @return this.polygonView
-	 */
+    public final MeshView getMeshView()
+    {
+        return this.meshView;
+    }
 
-	public final PolygonView getPolygonView() {
-		return this.polygonView;
-	}
+    /**
+     * Gets the polygon in the surfaceView.
+     * @return this.polygonView
+     */
 
-	/**
-	 * Getter.
-	 * 
-	 * @return the surface
-	 */
-	public final Surface getSurface() {
-		return this.surface;
-	}
+    public final PolygonView getPolygonView()
+    {
+        return this.polygonView;
+    }
 
-	/**
-	 * Sets the material of the surfaceMesh.
-	 * 
-	 * @param material
-	 *            The material to be used.
-	 */
+    /**
+     * Getter.
+     * @return the surface
+     */
+    public final Surface getSurface()
+    {
+        return this.surface;
+    }
 
-	public final void setMaterial(final Material material) {
-		this.appearance.setMaterial(material);
+    /**
+     * Sets the material of the surfaceMesh.
+     * @param material
+     *            The material to be used.
+     */
 
-	}
+    public final void setMaterial(final Material material)
+    {
+        this.appearance.setMaterial(material);
 
-	/**
-	 * Sets the mesh of triangles in the surfaceView.
-	 * 
-	 * @param meshViewIn
-	 *            The mesh of triangles to be displayed.
-	 */
+    }
 
-	public final void setMeshView(final MeshView meshViewIn) {
-		this.meshView = meshViewIn;
-		this.setGeometry(this.meshView);
-	}
+    /**
+     * Sets the mesh of triangles in the surfaceView.
+     * @param meshViewIn
+     *            The mesh of triangles to be displayed.
+     */
 
-	/**
-	 * Sets the polygon in the surfaceView.
-	 * 
-	 * @param polygonViewIn
-	 *            The polygon to be displayed.
-	 */
+    public final void setMeshView(final MeshView meshViewIn)
+    {
+        this.meshView = meshViewIn;
+        this.setGeometry(this.meshView);
+    }
 
-	public final void setPolygonView(final PolygonView polygonViewIn) {
-		this.polygonView = polygonViewIn;
-		this.setGeometry(this.polygonView);
-	}
+    /**
+     * Sets the polygon in the surfaceView.
+     * @param polygonViewIn
+     *            The polygon to be displayed.
+     */
 
-	/**
-	 * Setter.
-	 * 
-	 * @param surfaceIn
-	 *            the surface to set
-	 */
-	public final void setSurface(final Surface surfaceIn) {
-		this.surface = surfaceIn;
-	}
+    public final void setPolygonView(final PolygonView polygonViewIn)
+    {
+        this.polygonView = polygonViewIn;
+        this.setGeometry(this.polygonView);
+    }
 
-	
+    /**
+     * Setter.
+     * @param surfaceIn
+     *            the surface to set
+     */
+    public final void setSurface(final Surface surfaceIn)
+    {
+        this.surface = surfaceIn;
+    }
 
 }
