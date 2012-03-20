28d493998870465ad06f229c07e0b090a2670345
Camille Bouquet <bouquetcamille@hotmail.fr>
2012-03-20 18:21:23 +0100

    Saves step 7 results in lastResult.stl file in default folder and fixes little bug in isletSelection tree

--- src/fr/nantes1900/view/isletselection/GlobalTreeView.java
+++ src/fr/nantes1900/view/isletselection/GlobalTreeView.java
@@ -15,120 +15,133 @@ import javax.swing.tree.DefaultMutableTreeNode;
  */
 public class GlobalTreeView extends JPanel {
 
-    /**
-     * Version ID.
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 * Version ID.
+	 */
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * The JTree displaying the tree.
-     */
-    private JTree tree;
+	/**
+	 * The JTree displaying the tree.
+	 */
+	private JTree tree;
 
-    /**
-     * The ScrollPane containing the tree.
-     */
-    private JScrollPane spTree;
+	/**
+	 * The ScrollPane containing the tree.
+	 */
+	private JScrollPane spTree;
 
-    /**
-     * Empty constructor.
-     */
-    public GlobalTreeView() {
-        this.setLayout(new BorderLayout());
-        this.spTree = new JScrollPane();
-        this.add(this.spTree, BorderLayout.CENTER);
-    }
+	/**
+	 * Empty constructor.
+	 */
+	public GlobalTreeView() {
+		this.setLayout(new BorderLayout());
+		this.spTree = new JScrollPane();
+		this.add(this.spTree, BorderLayout.CENTER);
+	}
 
-    /**
-     * From a directory, builds the JTree of the contained STL files, and
-     * displays it.
-     * @param newDirectory
-     *            the name of the directory
-     */
-    public final void displayDirectory(final File newDirectory) {
-        FileNode newDirectoryNode = new FileNode(newDirectory);
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode(
-                newDirectoryNode);
-        this.fillMyTree(root, newDirectoryNode);
+	/**
+	 * From a directory, builds the JTree of the contained STL files, and
+	 * displays it.
+	 * 
+	 * @param newDirectory
+	 *            the name of the directory
+	 */
+	public final void displayDirectory(final File newDirectory) {
+		FileNode newDirectoryNode = new FileNode(newDirectory);
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode(
+				newDirectoryNode);
+		this.fillMyTree(root, newDirectoryNode);
 
-        this.tree = new JTree(root);
-        this.spTree.setViewportView(this.tree);
-    }
+		this.tree = new JTree(root);
+		this.spTree.setViewportView(this.tree);
+	}
 
-    /**
-     * Builds a tree from the content of the directory : if there is other
-     * directory, calls this method, and if there is STL files, adds it to the
-     * tree.
-     * @param root
-     *            the treeNode where will be returned the tree
-     * @param newDirectoryNode
-     *            the directory to explore
-     */
-    public final void fillMyTree(final DefaultMutableTreeNode root,
-            final FileNode newDirectoryNode) {
-        List<File> childrenFiles = Arrays.asList(newDirectoryNode.listFiles());
+	/**
+	 * Builds a tree from the content of the directory : if there is other
+	 * directory, calls this method, and if there is STL files, adds it to the
+	 * tree.
+	 * 
+	 * @param root
+	 *            the treeNode where will be returned the tree
+	 * @param newDirectoryNode
+	 *            the directory to explore
+	 */
+	public final void fillMyTree(final DefaultMutableTreeNode root,
+			final FileNode newDirectoryNode) {
+		// To improve to show only folders containing .stl files
+		// Maybe with a return node to add only if it is a file or is a valid folder
+		if (newDirectoryNode.listFiles() != null) {
+			List<File> childrenFiles = Arrays.asList(newDirectoryNode
+					.listFiles());
 
-        for (File file : childrenFiles) {
-            FileNode currentNode = new FileNode(file);
-            DefaultMutableTreeNode child = new DefaultMutableTreeNode(
-                    currentNode);
+			for (File file : childrenFiles) {
+				FileNode currentNode = new FileNode(file);
+				DefaultMutableTreeNode child = new DefaultMutableTreeNode(
+						currentNode);
 
-            // If it is a directory, search for others STL files inside of it.
-            if (file.isDirectory()) {
-                root.add(child);
-                this.fillMyTree(child, currentNode);
+				// If it is a directory, search for others STL files inside of
+				// it.
+				if (file.isDirectory()) {
+					root.add(child);
+					this.fillMyTree(child, currentNode);
 
-                // Displays the file in the tree only if it is a STL file.
-            } else if (currentNode.toString().endsWith("stl")) {
-                root.add(child);
-            }
-        }
-    }
+					// Displays the file in the tree only if it is a STL file.
+				} else if (currentNode.toString().endsWith("stl")) {
+					root.add(child);
+				}
+			}
+		}
+	}
 
-    /**
-     * Getter.
-     * @return the JTree
-     */
-    public final JTree getTree() {
-        return this.tree;
-    }
+	/**
+	 * Getter.
+	 * 
+	 * @return the JTree
+	 */
+	public final JTree getTree() {
+		return this.tree;
+	}
 
-    /**
-     * Implements an extension of the File with an overriden method toString
-     * used to display correctly the name of the file in the JTree.
-     * @author Luc Jallerat
-     */
-    public class FileNode extends File {
+	/**
+	 * Implements an extension of the File with an overriden method toString
+	 * used to display correctly the name of the file in the JTree.
+	 * 
+	 * @author Luc Jallerat
+	 */
+	public class FileNode extends File {
 
-        /**
-         * Serial version UID.
-         */
-        private static final long serialVersionUID = 1L;
+		/**
+		 * Serial version UID.
+		 */
+		private static final long serialVersionUID = 1L;
 
-        /**
-         * Constructor.
-         * @param file
-         *            the file object
-         */
-        public FileNode(final File file) {
-            super(file.getAbsolutePath());
-        }
+		/**
+		 * Constructor.
+		 * 
+		 * @param file
+		 *            the file object
+		 */
+		public FileNode(final File file) {
+			super(file.getAbsolutePath());
+		}
 
-        /**
-         * Returns the complete name of the file.
-         * @return the name of the file
-         */
-        public final String getEntireName() {
-            return super.toString();
-        }
+		/**
+		 * Returns the complete name of the file.
+		 * 
+		 * @return the name of the file
+		 */
+		public final String getEntireName() {
+			return super.toString();
+		}
 
-        /**
-         * Returns the short name of the file to display it in the JTree.
-         * @return the short name of the file
-         */
-        @Override
-        public final String toString() {
-            return super.getName();
-        }
-    }
+		/**
+		 * Returns the short name of the file to display it in the JTree.
+		 * 
+		 * @return the short name of the file
+		 */
+		@Override
+		public final String toString() {
+			return super.getName();
+		}
+	}
 }
